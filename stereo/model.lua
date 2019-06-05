66a83aa027b4d170a4eda1b284d61f2a69761c3c
beaupreda <david-alexandre.beaupre@polymtl.ca>
2019-06-05 14:57:23 -0400

    model refactor + added comments

--- stereo/model.lua
+++ stereo/model.lua
@@ -7,41 +7,45 @@ require 'nn'
 require 'cunn'
 
 function create_model(max_dips, nChannel)
-	local m = nn.Sequential()
+	-- whole network
+	local model = nn.Sequential()
+	-- base = combination of left and right network
+	local base = nn.ParallelTable()
+	local left = nn.Sequential()
 
-	local bottom = nn.ParallelTable()
-	local bottom_left = nn.Sequential()
-	-- building block
+	-- building block (convolution, batch normalization and ReLU activation)
 	local function ConvBNReLU(nInputPlane, nOutputPlane, kw, kh)
-      bottom_left:add(nn.SpatialConvolution(nInputPlane, nOutputPlane, kw, kh))
-	  bottom_left:add(nn.SpatialBatchNormalization(nOutputPlane,1e-3))
-	  bottom_left:add(nn.ReLU(true))
-	  return bottom_left
+      left:add(nn.SpatialConvolution(nInputPlane, nOutputPlane, kw, kh))
+	  left:add(nn.SpatialBatchNormalization(nOutputPlane, 1e-3))
+	  left:add(nn.ReLU(true))
+	  return left
 	end
 
 	local c = nChannel or 3
-	ConvBNReLU(c,32, 7,7):add(nn.Dropout(0.2))
-	ConvBNReLU(32,32,7,7):add(nn.Dropout(0.5))
-	ConvBNReLU(32,64,7,7):add(nn.Dropout(0.5))
-	ConvBNReLU(64,64,7,7):add(nn.Dropout(0.5))
-	ConvBNReLU(64,64,7,7):add(nn.Dropout(0.5))
-    bottom_left:add(nn.SpatialConvolution(64, 64, 7, 7))
-	bottom_left:add(nn.SpatialBatchNormalization(64,1e-3))
+	-- 5 layers for feature extraction
+	ConvBNReLU(c, 32, 7, 7):add(nn.Dropout(0.2))
+	ConvBNReLU(32, 32, 7, 7):add(nn.Dropout(0.5))
+	ConvBNReLU(32, 64, 7, 7):add(nn.Dropout(0.5))
+	ConvBNReLU(64, 64, 7, 7):add(nn.Dropout(0.5))
+	ConvBNReLU(64, 64, 7, 7):add(nn.Dropout(0.5))
+	-- no ReLU activation for last layer
+    left:add(nn.SpatialConvolution(64, 64, 7, 7))
+	left:add(nn.SpatialBatchNormalization(64, 1e-3))
 
-	local bottom_right = bottom_left:clone('weight','bias','gradWeight','gradBias')
-    bottom_left:add(nn.Transpose({2,3}, {3,4}))
-	bottom_left:add(nn.Reshape(1, 64))
+	-- left and right share parameters
+	local right = left:clone('weight', 'bias', 'gradWeight', 'gradBias')
+	left:add(nn.Transpose({2, 3}, {3, 4}))
+	-- left is a vector
+	left:add(nn.Reshape(1, 64))
+	-- right is a volume
+	right:add(nn.Reshape(64, max_dips))
+	base:add(left):add(right)
+	model:add(base)
+	-- correlation between left and right (for every disparity location)
+	model:add(nn.MM())
+	model:add(nn.Reshape(max_dips))
+	-- numerically stable probabilities
+	model:add(nn.LogSoftMax())
 
-	bottom_right:add(nn.Reshape(64,max_dips))
-
-	bottom:add(bottom_left):add(bottom_right)
-
-	m:add(bottom)
-	m:add(nn.MM())
-	m:add(nn.Reshape(max_dips))
-
-	m:add(nn.LogSoftMax())
-
-	return m
-
+	return model
 end
